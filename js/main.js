2c69350e97ada9cdf5413de20f322c8f381546be
Alexandre Gauthier <agauthier5@outlook.fr>
2024-02-07 11:43:15 +0100

    Ajout de la possibilité de changer le nom du joueur via une popUp, ainsi qu'un changement de couleur pour indiquer la personne qui joue et la personne qui gagne. Modification des couleurs pour correspondre avec les normes Who Can use.

--- js/main.js
+++ js/main.js
@@ -18,9 +18,27 @@ let nbScoreO = 0;
 // On déclare une variable qui va définir si on joue un X ou un O (1 - X 2 - O)
 let count = 1;
 
-
 // On count le nombre total de coup joué
 let countTotal = 0;
+
+// Je récupère mes box et nomJoueur 
+let boxJoueurUn = document.getElementById("boxJoueurUn")
+let boxJoueurDeux = document.getElementById("boxJoueurDeux")
+
+let nomJoueurUn = document.getElementById("nomJoueurUn")
+let nomJoueurDeux = document.getElementById("nomJoueurDeux")
+
+let inputJoueurUn = document.getElementById("inputJoueurUn")
+let inputJoueurDeux = document.getElementById("inputJoueurDeux")
+
+let popUp = document.querySelector(".popupConfigJeu")
+let popUpform = document.querySelector(".pop")
+
+let submit = document.getElementById("submitNomJoueur")
+let changeNom = document.getElementById("changerNom")
+
+let resetScore = document.getElementById("resetScore")
+
 
 // Function tictactoe 
 function tictactoe(box) {
@@ -52,53 +70,62 @@ function victoire() { // Gestion de la victoire
         [1, 5, 9], // diagonales
         [3, 5, 7]
     ];
-
+    
     for (const line of lines) {
         const [a, b, c] = line;
         const boxA = document.querySelector(`.box_${a}`);
         const boxB = document.querySelector(`.box_${b}`);
         const boxC = document.querySelector(`.box_${c}`);
-
+        
         if (boxA.textContent !== "" && boxA.textContent === boxB.textContent && boxB.textContent === boxC.textContent) {
             // Si il y a une victoire, ajoute la classe "green" au element gagnant
             boxA.classList.add("green")
             boxB.classList.add("green")
             boxC.classList.add("green")
-
+            
             return true;
         }
     }
-
+    
     return false; // Pas de victoire
 }
 
 // Function qui va gerer le texte en dessous de la partie
 function phrases(textun, textdeux) {
     if (victoire()) { // Si la function victoire est "true" alors
-        textun.textContent = `Joueur ${count === 1 ? 'O' : 'X'} remporte la partie !`;
-        textdeux.textContent = `Joueur ${count === 1 ? 'O' : 'X'} remporte la partie !`;
-
+        
         board.classList.add("fin"); // Class fin qui, en CSS, desactive le jeu
-
+        
         if(count === 2) {
             nbScoreX++
             scoreX.textContent = nbScoreX;
+            textun.textContent = `${inputJoueurUn.value} remporte la partie !`;
+            boxJoueurUn.classList.remove("tour")
+            boxJoueurUn.classList.add("victoire")
         }
         else {
             nbScoreO++
             scoreO.textContent = nbScoreO;
+            textdeux.textContent = `${inputJoueurDeux.value} remporte la partie !`;
+            boxJoueurDeux.classList.remove("tour")
+            boxJoueurDeux.classList.add("victoire")
         }
     }
     
     else if (countTotal === 9) { // Si le nombre total de coup est joué, alors fin de la partie.
         textun.textContent = "Fin de la partie ! Match nul !"
         textdeux.textContent = "Fin de la partie ! Match nul !"
+        boxJoueurUn.classList.remove("tour")
     }
     else if (count === 1) { // Sinon si count = 1 alors X
+        boxJoueurUn.classList.add("tour")
+        boxJoueurDeux.classList.remove("tour")
         textun.textContent = "Votre tour !"
         textdeux.textContent = "  "
     }
     else if (count === 2) { // Sinon par defaut ça sera O
+        boxJoueurUn.classList.remove("tour")
+        boxJoueurDeux.classList.add("tour")
         textun.textContent = "  "
         textdeux.textContent = "Votre tour !"
     } else {
@@ -108,16 +135,22 @@ function phrases(textun, textdeux) {
 
 // Function pour reset la partie et recommencer à jouer 
 function partieReset(parent) {
-
+    
     parent.querySelectorAll(".carree").forEach(function (validBox) {
-
+        
         // On retire toutes les class
         board.classList.remove("fin");
+        
+        boxJoueurUn.classList.remove("tour");
+        boxJoueurDeux.classList.remove("tour");
+        boxJoueurUn.classList.remove("victoire");
+        boxJoueurDeux.classList.remove("victoire");
+        
         validBox.classList.remove("croix");
         validBox.classList.remove("rond");
         validBox.classList.remove("green");
         validBox.textContent = "";
-
+        
         count = 1;
         countTotal = 0;
     });
@@ -150,3 +183,29 @@ recommencerBtn.addEventListener("click", function () {
     partieReset(board);
     phrases(phraseJoueurUn, phraseJoueurDeux);
 });
+
+changeNom.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    popUp.style.display = "inline";
+})
+
+submit.addEventListener("click", (event) => {
+
+        event.preventDefault();
+
+    if (inputJoueurUn.value !== inputJoueurDeux.value && inputJoueurUn.value !== "" && inputJoueurDeux.value !== "") {
+        nomJoueurUn.textContent = inputJoueurUn.value
+        nomJoueurDeux.textContent = inputJoueurDeux.value
+        popUp.style.display = "none";
+    } else if (inputJoueurUn === inputJoueurDeux) {
+        popUpform.style.backgroundColor = "red";
+    }
+})
+
+resetScore.addEventListener("click", function() {
+    nbScoreO = 0
+    nbScoreX = 0
+    scoreO.textContent = "0"
+    scoreX.textContent = "0"
+})
